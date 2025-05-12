9b31ed4c002cdb328c26a8785e262fed706ff80f
Terry <terrysistemas@gmail.com>
2025-05-12 12:50:31 -0500

    add Api

--- back-transportes/routes/api.php
+++ back-transportes/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::apiResource('empleados', EmpleadoController::class);
